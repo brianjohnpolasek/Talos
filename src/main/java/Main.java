53a36b04cba9ec9b19f542fc23f85e303faacf59
brianjohnpolasek <brianjohnpolasek@gmail.com>
2021-03-10 20:46:06 -0600

    Finalize ping pong command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,12 +12,6 @@ public class Main extends ListenerAdapter
 {
     public static void main(String[] args) throws LoginException
     {
-        /*
-        if (args.length < 1) {
-            System.out.println("You have to provide a token as first argument!");
-            System.exit(1);
-        }*/
-
         config bot = new config();
 
         JDABuilder.createLight(bot.getToken(), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)

bf25f03eee4b438e1c57bc248ea5a6bb37fa7769
brianjohnpolasek <brianjohnpolasek@gmail.com>
2021-03-29 15:50:54 -0500

    Extend commands to include prefix.

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -5,18 +5,19 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Commands extends ListenerAdapter {
+
+    String prefix = "%";
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
         Message msg = event.getMessage();
-        if (msg.getContentRaw().equals("!ping"))
+        if (msg.getContentRaw().equals(prefix + "ping"))
         {
             MessageChannel channel = event.getChannel();
             long time = System.currentTimeMillis();
             channel.sendMessage("Pong!") /* => RestAction<Message> */
-                    .queue(response /* => Message */ -> {
-                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                    });
+                    .queue(response /* => Message */ -> response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue());
         }
     }
 }

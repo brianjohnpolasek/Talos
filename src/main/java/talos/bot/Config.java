857fb24465294c15fb2bd2cfd0f8e8e3b8575101
brianjohnpolasek <brianjohnpolasek@gmail.com>
2021-04-08 20:12:32 -0500

    Test env

--- src/main/java/talos/bot/Config.java
+++ src/main/java/talos/bot/Config.java
@@ -1,12 +1,13 @@
 package talos.bot;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    //private static final Dotenv dotenv = Dotenv.load();
 
     public static String get(String key) {
-        return dotenv.get(key);
+        //return dotenv.get(key);
+        return System.getenv("TOKEN");
     }
 }

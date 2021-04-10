460f5770fc9ad8c9e0e844a90412b386fb44c44a
brianjohnpolasek <brianjohnpolasek@gmail.com>
2021-04-09 19:33:00 -0500

    Refactor text commands

--- src/main/java/talos/bot/Config.java
+++ src/main/java/talos/bot/Config.java
@@ -4,17 +4,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    //private static final Dotenv dotenv = Dotenv.load();
+
+    private static final boolean isRemote = System.getenv("TOKEN") == null;
 
     public static String get(String key) {
 
-        String systemEnv = System.getenv().get(key);
-
-        if (systemEnv.isEmpty()) {
-            return dotenv.get(key);
+        if (isRemote) {
+            return Dotenv.load().get(key);
         }
         else {
-            return systemEnv;
+            return System.getenv(key);
         }
     }
 }

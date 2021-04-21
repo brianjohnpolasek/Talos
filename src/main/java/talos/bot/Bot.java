b77637bd71c3de06f340e9dbb8a2da6a2c84c432
brianjohnpolasek <brianjohnpolasek@gmail.com>
2021-04-21 16:12:47 -0500

    Remove default status.

--- src/main/java/talos/bot/Bot.java
+++ src/main/java/talos/bot/Bot.java
@@ -1,7 +1,6 @@
 package talos.bot;
 
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
@@ -17,7 +16,6 @@ public class Bot {
                 GatewayIntent.GUILD_VOICE_STATES)
                 .enableCache(CacheFlag.VOICE_STATE)
                 .addEventListeners(new DiscordListener())
-                .setActivity(Activity.watching(Config.get("DEFAULT_STATUS")))
                 .build();
     }
 
